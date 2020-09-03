5c38a9346ea420244694337b8d75ee958f824aef
Aleksandr <mail.bolshakov@gmail.com>
2020-09-03 16:33:07 +0300

    camera roll is interface

--- lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Client.java
+++ lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Client.java
@@ -2,7 +2,12 @@ package pro.bolshakov.geekbrains.springlevelone;
 
 public class Client {
     public static void main(String[] args) {
-        Camera camera = new Camera(new CameraRoll());
+        ColorCameraRoll colorCameraRoll = new ColorCameraRoll();
+        Camera camera = new Camera(colorCameraRoll);
+        camera.doPhotograph();
+
+        BlackWhiteCameraRoll blackWhiteCameraRoll = new BlackWhiteCameraRoll();
+        camera.setCameraRoll(blackWhiteCameraRoll);
         camera.doPhotograph();
     }
 }
