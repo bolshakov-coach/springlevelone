d629d1d60c480e4af018b00975d11e597f336cd2
Aleksandr <mail.bolshakov@gmail.com>
2020-09-03 17:30:46 +0300

    added annotation scan for java config

--- lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/AppConfig.java
+++ lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/AppConfig.java
@@ -1,15 +1,12 @@
 package pro.bolshakov.geekbrains.springlevelone;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("pro.bolshakov.geekbrains.springlevelone")
 public class AppConfig {
-
-    @Bean(name = "cameraRoll")
-    public CameraRoll cameraRoll(){
-        return new ColorCameraRoll();
-    }
 
     @Bean(name = "camera")
     public Camera camera(CameraRoll cameraRoll){
