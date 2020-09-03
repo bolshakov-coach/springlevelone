42de2519900b0f25a4cfe1b110fd01d59ad73fdf
Aleksandr <mail.bolshakov@gmail.com>
2020-09-03 16:47:28 +0300

    Spring context by XML

--- lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Camera.java
+++ lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Camera.java
@@ -1,22 +1,5 @@
 package pro.bolshakov.geekbrains.springlevelone;
 
-public class Camera {
-    private CameraRoll cameraRoll;
-
-    public Camera(CameraRoll cameraRoll) {
-        this.cameraRoll = cameraRoll;
-    }
-
-    public void doPhotograph(){
-        System.out.println("Chick - chick");
-        cameraRoll.processing();
-    }
-
-    public CameraRoll getCameraRoll() {
-        return cameraRoll;
-    }
-
-    public void setCameraRoll(CameraRoll cameraRoll) {
-        this.cameraRoll = cameraRoll;
-    }
+public interface Camera {
+    void doPhotograph();
 }
