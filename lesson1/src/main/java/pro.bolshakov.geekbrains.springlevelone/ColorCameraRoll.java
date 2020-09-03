27f58e78887b4d1774a6bc8aa7b57a670d2cfde6
Aleksandr <mail.bolshakov@gmail.com>
2020-09-03 17:27:34 +0300

    added annotation scanning into XML config

--- lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/ColorCameraRoll.java
+++ lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/ColorCameraRoll.java
@@ -1,5 +1,8 @@
 package pro.bolshakov.geekbrains.springlevelone;
 
+import org.springframework.stereotype.Component;
+
+@Component("cameraRoll")
 public class ColorCameraRoll implements CameraRoll {
     @Override
     public void processing() {
