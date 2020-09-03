082fcd0c6574b29a6058571861ee1acb35ec3069
Aleksandr <mail.bolshakov@gmail.com>
2020-09-03 12:59:13 +0300

    inject roll through constructor and setter

--- lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Camera.java
+++ lesson1/src/main/java/pro.bolshakov.geekbrains.springlevelone/Camera.java
@@ -1,10 +1,22 @@
 package pro.bolshakov.geekbrains.springlevelone;
 
 public class Camera {
-    private CameraRoll cameraRoll = new CameraRoll();
+    private CameraRoll cameraRoll;
+
+    public Camera(CameraRoll cameraRoll) {
+        this.cameraRoll = cameraRoll;
+    }
 
     public void doPhotograph(){
         System.out.println("Chick - chick");
         cameraRoll.processing();
+    }
+
+    public CameraRoll getCameraRoll() {
+        return cameraRoll;
+    }
+
+    public void setCameraRoll(CameraRoll cameraRoll) {
+        this.cameraRoll = cameraRoll;
     }
 }
