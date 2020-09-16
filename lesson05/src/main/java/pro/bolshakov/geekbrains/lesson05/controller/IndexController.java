75a0d66da003ef240a0336da46bc925f1502662c
Aleksandr <mail.bolshakov@gmail.com>
2020-09-16 12:08:37 +0300

    additional example

--- lesson05/src/main/java/pro/bolshakov/geekbrains/lesson05/controller/IndexController.java
+++ lesson05/src/main/java/pro/bolshakov/geekbrains/lesson05/controller/IndexController.java
@@ -3,6 +3,7 @@ package pro.bolshakov.geekbrains.lesson05.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pro.bolshakov.geekbrains.lesson05.domain.User;
 
 import java.util.UUID;
 
@@ -12,7 +13,8 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("message", "My message plus random UUID 1-> " + UUID.randomUUID());
-        model.addAttribute("title", "Title of Index Page");
+        model.addAttribute("title", "Spring Level one");
+        model.addAttribute("user", new User("Vasiliy"));
         return "index";
     }
 
