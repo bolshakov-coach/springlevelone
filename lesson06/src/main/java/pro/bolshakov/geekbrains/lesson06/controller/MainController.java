88f51e042d28a1b728c6175b7b1dd5237c1edef8
Aleksandr <mail.bolshakov@gmail.com>
2020-09-19 19:07:33 +0300

    example with static resources

--- lesson06/src/main/java/pro/bolshakov/geekbrains/lesson06/controller/MainController.java
+++ lesson06/src/main/java/pro/bolshakov/geekbrains/lesson06/controller/MainController.java
@@ -3,14 +3,12 @@ package pro.bolshakov.geekbrains.lesson06.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller
 public class MainController {
-    @GetMapping("/**")
-    @ResponseBody
+
+    @GetMapping("/")
     public String helloWorld() {
-        return "Hello, it is my Spring Boot App";
+        return "index";
     }
 }
