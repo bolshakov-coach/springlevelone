4d6baff5a53d8ffbecbc648832df449cfcffab5a
Aleksandr <mail.bolshakov@gmail.com>
2020-09-27 16:14:28 +0300

    add simple authentication

--- lesson08/src/main/java/pro/bolshakov/geekbrains/springlevelone/lesson08/controller/UserController.java
+++ lesson08/src/main/java/pro/bolshakov/geekbrains/springlevelone/lesson08/controller/UserController.java
@@ -5,8 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pro.bolshakov.geekbrains.springlevelone.lesson08.domain.User;
 import pro.bolshakov.geekbrains.springlevelone.lesson08.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -19,8 +23,23 @@ public class UserController {
     }
 
     @GetMapping
-    public String getList(Model model){
-        model.addAttribute("users", userService.getAll());
+    public String getList(Model model,
+                          @RequestParam(required = false) String username,
+                          @RequestParam(required = false) String password){
+        List<User> userList = new ArrayList<>();
+
+        if(username != null){
+            boolean auth = userService.auth(username, password);
+            if(auth){
+                userList = userService.getAll();
+                System.out.println("You are authenticated");
+            }
+        }
+        if(userList.isEmpty()){
+            System.out.println("You are not authenticated");
+        }
+
+        model.addAttribute("users", userList);
         return "user-list";
     }
 
