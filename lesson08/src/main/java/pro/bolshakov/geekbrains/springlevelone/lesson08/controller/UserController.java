61b0be9a61e6300d62b19120a372497b8363a636
Aleksandr <mail.bolshakov@gmail.com>
2020-09-27 17:17:38 +0300

    add filter with auth

--- lesson08/src/main/java/pro/bolshakov/geekbrains/springlevelone/lesson08/controller/UserController.java
+++ lesson08/src/main/java/pro/bolshakov/geekbrains/springlevelone/lesson08/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pro.bolshakov.geekbrains.springlevelone.lesson08.config.UserInfo;
 import pro.bolshakov.geekbrains.springlevelone.lesson08.domain.Role;
 import pro.bolshakov.geekbrains.springlevelone.lesson08.domain.User;
 import pro.bolshakov.geekbrains.springlevelone.lesson08.service.UserService;
@@ -18,32 +19,32 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final UserInfo userInfo;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserInfo userInfo) {
         this.userService = userService;
+        this.userInfo = userInfo;
     }
 
     @GetMapping
     public String getList(Model model,
                           @RequestParam(required = false) String username,
-                          @RequestParam(required = false) String password){
+                          @RequestParam(required = false) String password) {
         List<User> userList = new ArrayList<>();
-        User authUser = userService.auth(username, password);
-        if(authUser != null){
-            if(Role.ADMIN.equals(authUser.getRole())){
-                System.out.println("You have permission (authorization) ");
-                userList = userService.getAll();
-            }
-            else {
-                System.out.println("You do not have permission (authorization)");
-            }
+        if(userInfo.hasRole(Role.ADMIN)){
+            System.out.println("You have permission (authorization) ");
+            userList = userService.getAll();
         }
+        else{
+            System.out.println("You do not have permission (authorization)");
+        }
+
         model.addAttribute("users", userList);
         return "user-list";
     }
 
     @GetMapping("/{id}")
-    public String getById(Model model, @PathVariable Long id){
+    public String getById(Model model, @PathVariable Long id) {
         User byId = userService.getById(id);
         model.addAttribute("user", byId == null ? new User() : byId);
         return "user";
